89068645100c4803e51d549e5c68677844bc6059
HI <87406580+sachusamu-c@users.noreply.github.com>
2021-09-26 10:43:04 +0530

    𝘴𝘴 ᴄͥʀⷮᴇᷤᴀⷨᴛⷷɪᴏɴ

--- plugins/git.js
+++ plugins/git.js
@@ -18,7 +18,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater sachusamu*
 *owner number wa.me/919744474996*
 
 *Bot making video : https://youtu.be/fd0KPblop-k*
