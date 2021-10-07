e4c5fcc95fe771ac520e324772f0eb3ae2610f4c
HI <87406580+sachusamu-c@users.noreply.github.com>
2021-10-07 20:50:56 +0530

    🍃ʀᴏᴄͥᴋⷮsᷤᴇⷨʀⷷ ʙᴏᴛ🦋

--- plugins/setvar.js
+++ plugins/setvar.js
@@ -18,7 +18,7 @@ Asena.addCommand({pattern: 'setvar', fromMe: false, desc: Lang.WP}, (async (mess
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━💘SETVAR💘━━⛦
-𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Amalser 𝐛𝐨𝐭.
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 𝐊𝐈𝐋𝐋𝐀𝐃𝐈  𝐛𝐨𝐭.
   
 *ẉa.me/➳Setvar*
 ■□■□■□■□■□■□■□■□■□■□
@@ -90,9 +90,9 @@ All setvars are
  To get a song when your number is mentioned
  .setvar TAG_REPPLY:your jid 
 
-     *happy using Amalser bot ❣️*
+     *happy using Rockser bot ❣️*
 
-*Editing And Devaloper : Amal -Amalser*
+*Editing And Devaloper : sachusamu -Rockser*
 
 *Erorr Fixing : Farhan -Julie Mwol 💌*
 
